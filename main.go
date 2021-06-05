defbaa259e680b9b099fbf697a4be531f2928331
Dave <daveg7lee@gmail.com>
2021-06-05 15:46:38 +0900

    [REFACTORING] Refactoring part Two

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "github.com/daveg7lee/kangaroocoin/blockchain"
+import (
+	"fmt"
+
+	"github.com/daveg7lee/kangaroocoin/blockchain"
+)
 
 func main() {
 	chain := blockchain.GetBlockchain()
+	chain.AddBlock("Second Block")
+	blocks := chain.AllBlocks()
+	for _, block := range blocks {
+		fmt.Printf("Data: %s\n", block.Data)
+		fmt.Printf("Hash: %s\n", block.Hash)
+		fmt.Printf("Prev Hash: %s\n", block.PrevHash)
+	}
 }
