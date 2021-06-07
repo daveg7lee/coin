6baa25eed2bfea2a8e3869eeef3db1220e9de0db
Dave <daveg7lee@gmail.com>
2021-06-07 16:58:51 +0900

    [ADD] Rest API Setup

--- main.go
+++ main.go
@@ -1,7 +1,37 @@
 package main
 
-import "github.com/daveg7lee/kangaroocoin/explorer"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/daveg7lee/kangaroocoin/utils"
+)
+
+const port string = ":4000"
+
+type URLDescription struct {
+	URL         string
+	Method      string
+	Description string
+}
+
+func documentation(rw http.ResponseWriter, r *http.Request) {
+	data := []URLDescription{
+		{
+			URL:         "/",
+			Method:      "GET",
+			Description: "See Documentation",
+		},
+	}
+	b, err := json.Marshal(data)
+	utils.HandleErr(err)
+	rw.Write(b)
+}
 
 func main() {
-	explorer.Start()
+	http.HandleFunc("/", documentation)
+	fmt.Printf("Server running on http://localhost%s\n", port)
+	log.Fatal(http.ListenAndServe(port, nil))
 }
