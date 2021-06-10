bb5716c77c1fda82588025f361c0c62b978c6b2b
Dave <daveg7lee@gmail.com>
2021-06-10 20:04:11 +0900

    [ADD] Comments

--- cli/cli.go
+++ cli/cli.go
@@ -9,6 +9,7 @@ import (
 	"github.com/daveg7lee/kangaroocoin/rest"
 )
 
+// show usage of cli
 func usage() {
 	fmt.Printf("Welcome to Kangaroo Coin\n\n")
 	fmt.Printf("Please use the follow flags:\n\n")
@@ -17,25 +18,34 @@ func usage() {
 	os.Exit(0)
 }
 
+// start cli
 func Start() {
+	// check len of arg
 	if len(os.Args) == 1 {
+		// show usage is there are no flag
 		usage()
 	}
 
+	// make flags
 	port := flag.Int("port", 4000, "Set port of the server")
 	mode := flag.String("mode", "rest", "Choose between 'html' and 'rest'")
-
+	// parse flags
 	flag.Parse()
 
+	// check mode
 	switch *mode {
 	case "rest":
+		// start rest api
 		rest.Start(*port)
 	case "html":
+		// start html explorer
 		explorer.Start(*port)
 	case "all":
+		// start both of them
 		go rest.Start(*port + 1000)
 		explorer.Start(*port)
 	default:
+		// show usage if mode is wrong
 		usage()
 	}
 }
