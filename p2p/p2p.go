bda9c4319b4181365dfd705d41bc9cc897706d33
Dave <daveg7lee@gmail.com>
2021-07-20 19:20:07 +0900

    [ADD] Connections

--- p2p/p2p.go
+++ p2p/p2p.go
@@ -1,13 +1,13 @@
 package p2p
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/daveg7lee/kangaroocoin/utils"
 	"github.com/gorilla/websocket"
 )
 
+var conns []*websocket.Conn
 var upgrader = websocket.Upgrader{}
 
 func Upgrade(rw http.ResponseWriter, r *http.Request) {
@@ -15,14 +15,17 @@ func Upgrade(rw http.ResponseWriter, r *http.Request) {
 		return true
 	}
 	conn, err := upgrader.Upgrade(rw, r, nil)
+	conns = append(conns, conn)
 	utils.HandleErr(err)
 	for {
 		_, payload, err := conn.ReadMessage()
 		if err != nil {
 			break
 		}
-		fmt.Printf("Just got: %s\n", payload)
-		message := fmt.Sprintf("We also think that: %s\n", payload)
-		utils.HandleErr(conn.WriteMessage(websocket.TextMessage, []byte(message)))
+		for _, aConn := range conns {
+			if aConn != conn {
+				utils.HandleErr(aConn.WriteMessage(websocket.TextMessage, payload))
+			}
+		}
 	}
 }
