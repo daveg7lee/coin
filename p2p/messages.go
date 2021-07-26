430a7bf7eb1716f633295bc48f8b812803010b57
Dave <daveg7lee@gmail.com>
2021-07-26 21:32:56 +0900

    [ADD] Recap

--- p2p/messages.go
+++ p2p/messages.go
@@ -2,6 +2,7 @@ package p2p
 
 import (
 	"encoding/json"
+	"fmt"
 
 	"github.com/daveg7lee/kangaroocoin/blockchain"
 	"github.com/daveg7lee/kangaroocoin/utils"
@@ -29,6 +30,7 @@ func makeMessage(kind MessageKind, payload interface{}) []byte {
 }
 
 func sendNewestBlock(p *peer) {
+	fmt.Printf("Sending newest block to %s\n", p.key)
 	b, err := blockchain.FindBlock(blockchain.Blockchain().NewestHash)
 	utils.HandleErr(err)
 	m := makeMessage(NewestBlockMessage, b)
@@ -48,18 +50,23 @@ func sendAllBlocks(p *peer) {
 func handleMsg(m *Message, p *peer) {
 	switch m.Kind {
 	case NewestBlockMessage:
+		fmt.Printf("Received the newest block from %s\n", p.key)
 		var payload blockchain.Block
 		json.Unmarshal(m.Payload, &payload)
 		b, err := blockchain.FindBlock(blockchain.Blockchain().NewestHash)
 		utils.HandleErr(err)
 		if payload.Height >= b.Height {
+			fmt.Printf("Requesting all blocks from %s\n", p.key)
 			requestAllBlocks(p)
 		} else {
+			fmt.Printf("Sending newest block to %s\n", p.key)
 			sendNewestBlock(p)
 		}
 	case AllBlocksRequestMessage:
+		fmt.Printf("%s wants all the blocks\n", p.key)
 		sendAllBlocks(p)
 	case AllBlockResponseMessage:
+		fmt.Printf("Received all the blocks from %s\n", p.key)
 		var payload []*blockchain.Block
 		err := json.Unmarshal(m.Payload, &payload)
 		utils.HandleErr(err)
